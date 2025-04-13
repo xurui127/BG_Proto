0878e10709b3595ed92c212f33c19951d9df5892
felixsoum <felix@felixsoum.com>
2025-04-12 21:31:49 -0400

    Prevent pan if mouse at bottom of screen

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -26,6 +26,7 @@ public class CameraHandler : MonoBehaviour
 
 
     internal static UnityEvent<bool> OnCameraMovementToggleEvent = new();
+    private bool isPanning;
 
     private void OnEnable()
     {
@@ -50,26 +51,34 @@ public class CameraHandler : MonoBehaviour
     {
         if (!canMoveCamera) return;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.y > CardUI.yOffset)
         {
+            isPanning = true;
             dragOrigin = Input.mousePosition;
             ResetCameraFollowTarget();
         }
 
-        if (Input.GetMouseButton(0))
+        if (isPanning)
         {
-            Vector3 mouseDelta = Input.mousePosition - dragOrigin;
-            Vector3 move = new Vector3(-mouseDelta.x, 0, -mouseDelta.y) * panSpeed * Time.deltaTime;
-            Vector3 newPos = cam.transform.position + move;
-
-            newPos.x = Mathf.Clamp(newPos.x, panRangeMin, panRangeMax);
-            newPos.z = Mathf.Clamp(newPos.z, panRangeMin, panRangeMax);
-
-
-            if (Mathf.Abs(newPos.x) <= boundX && Mathf.Abs(newPos.z) <= boundZ)
+            if (Input.GetMouseButton(0))
             {
-                cam.transform.position = newPos;
-                dragOrigin = Input.mousePosition;
+                Vector3 mouseDelta = Input.mousePosition - dragOrigin;
+                Vector3 move = new Vector3(-mouseDelta.x, 0, -mouseDelta.y) * panSpeed * Time.deltaTime;
+                Vector3 newPos = cam.transform.position + move;
+
+                newPos.x = Mathf.Clamp(newPos.x, panRangeMin, panRangeMax);
+                newPos.z = Mathf.Clamp(newPos.z, panRangeMin, panRangeMax);
+
+
+                if (Mathf.Abs(newPos.x) <= boundX && Mathf.Abs(newPos.z) <= boundZ)
+                {
+                    cam.transform.position = newPos;
+                    dragOrigin = Input.mousePosition;
+                }
+            }
+            else
+            {
+                isPanning = false;
             }
         }
     }
@@ -127,12 +136,12 @@ public class CameraHandler : MonoBehaviour
     internal void LockCameraControl()
     {
         allowCameraControl = false;
-        canMoveCamera = false; 
+        canMoveCamera = false;
     }
 
     internal void UnlockCameraControl()
     {
         allowCameraControl = true;
-        canMoveCamera = true; 
+        canMoveCamera = true;
     }
 }
